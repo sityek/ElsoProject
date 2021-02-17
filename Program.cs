67e9198824f69e8001459c14ef1cafd248fa03a5
Sityek <sityu@live.com>
2021-02-17 18:28:51 +0100

    Singleton program!

--- Program.cs
+++ Program.cs
@@ -2,12 +2,44 @@
 
 namespace ElsoProject
 {
-    class Program
+    public class Singleton
     {
-        static void Main(string[] args)
+        //static referencia egy példányra
+        private static Singleton _instance;
+
+        //a privát konstruktor megtiltja a new operátoros közvetlen példányosítást
+        private Singleton()
         {
-            
-            
+            Console.WriteLine("Singleton példányosítva");
+        }
+
+        //Példány lekérdezése
+        //Lehetne akár Property is
+        public static Singleton GetInstance()
+        {
+            //példányosítás első alkalommal, amikor használjuk
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }                
+
+            return _instance;
         }
     }
+
+        class Program
+    {
+        static void Main(string[] args)
+            {
+                Singleton test1 = Singleton.GetInstance();
+                Singleton test2 = Singleton.GetInstance();
+
+                if (test1 == test2)
+                {
+                    Console.WriteLine("Tényleg egy példány van belőle!");
+                }
+
+                Console.ReadKey();
+            }
+        }
 }
